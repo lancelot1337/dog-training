6c0cd93b849660531da07852d274ad9a837c6b38
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-01 16:11:55 +0530

    Add _id to dogs schema

--- models/dog.js
+++ models/dog.js
@@ -1,13 +1,14 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 //blog Schema
-const dogSchema=mongoose.Schema({
-	name: String,
-	age: String,
-	breed: String,
-	gender: String,
-	HandlerName: String,
-	HandlerId: String,
-})
+const dogSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    age: String,
+    breed: String,
+    gender: String,
+    HandlerName: String,
+    HandlerId: String,
+});
 
-module.exports=mongoose.model('Dog' , dogSchema)
+module.exports = mongoose.model('Dog', dogSchema)
