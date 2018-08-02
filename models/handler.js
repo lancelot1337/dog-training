219d7867d74eeef533a1c82145a6ce25137f068e
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-02 23:36:04 +0530

    Add Handler model

--- models/handler.js
+++ models/handler.js
@@ -1,48 +1,17 @@
-const passportLocalMongoose=require('passport-local-mongoose');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({
-	username:{
-		type:String,
-		unique:'username must be unique',
-		required:'username must be mentioned',
-	},
-	displayName:String,
-	handlerPic:String,
-	dog:[{
-		type:mongoose.Schema.Types.ObjectId,
-		ref:'Dog'
-	}],
-	salt:String,
-	hash:String,
+const handlerSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+        type: String,
+        unique: 'username must be unique',
+        required: 'username must be mentioned',
+    },
+    displayName: String,
+    dog: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dog',
+    },
+}, { timestamp: true });
 
-},{timestamp:true})
-
-
-const crypto=require('crypto');
-
-//one way to save passwords
-
-// userSchema.pre('save', function (next) {
-//     var user = this;
-//     if (!user.isModified('password')) {return next()};
-//     bcrypt.hash(user.password,10).then((hashedPassword) => {
-//         user.password = hashedPassword;
-//         next();
-//     })
-// }, function (err) {
-//     next(err)
-// })
-
-
-userSchema.methods.setPassword=function(password){
-	this.salt=crypto.randomBytes(16).toString('hex')
-	this.hash=crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex')
-}
-
-userSchema.methods.comparePassword=function(password){
-    hash=crypto.pbkdf2Sync(password,this.salt,10000,512,'sha512').toString('hex')
-    return hash===this.hash;
-
-}
-module.exports=mongoose.model('User ', userSchema);
+module.exports = mongoose.model('Handler', handlerSchema)
