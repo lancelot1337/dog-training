1359c18592eb6fa758f5d01023f9640ee78ed37c
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-08 19:24:45 +0530

    Update handlerSchema

--- models/handler.js
+++ models/handler.js
@@ -12,6 +12,16 @@ const handlerSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Dog',
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: true
+    }
 }, { timestamp: true });
 
 module.exports = mongoose.model('Handler', handlerSchema)
