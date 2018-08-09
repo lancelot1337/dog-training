5d4d8cba1b117910f2d65b8976e6d8b69fcb787d
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-09 11:28:25 +0530

    Add /handler/login and merge with /handler/

--- api/routes/handlers.js
+++ api/routes/handlers.js
@@ -64,50 +64,154 @@ router.post('/', (req, res, next) => {
 
 router.post('/signup', (req, res, next) => {
     console.log('/handlers/signup POST logged');
-    Hander.find({
-        email: req.body.email
-    })
+    Dog.findById(req.body.dogId)
         .exec()
-        .then(handler => {
-            if (handler.length > 0) {
-                res.status(409).json({
-                    message: "Email is already registered, try logging in!"
+        .then(dog => {
+            if (!dog) {
+                return res.status(404).json({
+                    message: "Invalid dogId"
                 });
             }
-            else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
+            Handler.find({
+                email: req.body.email
+            })
+                .exec()
+                .then(handler => {
+                    if (handler.length > 0) {
+                        res.status(409).json({
+                            message: "Email is already registered"
                         });
                     }
                     else {
-                        const handler = new Handler({
-                            _id: new mongoose.Types.ObjectId,
-                            email: req.body.email,
-                            password: hash
-                        });
-                        handler.save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: "Handler created"
-                                });
-                            })
-                            .catch(err => {
-                                res.status(500).json({
+                        bcrypt.hash(req.body.password, 10, (err, hash) => {
+                            if (err) {
+                                return res.status(500).json({
                                     error: err
-                                })
-                            });
+                                });
+                            }
+                            else {
+                                const handler = new Handler({
+                                    _id: new mongoose.Types.ObjectId,
+                                    username: req.body.username,
+                                    displayName: req.body.displayName,
+                                    dog: req.body.dogId,
+                                    email: req.body.email,
+                                    password: hash
+                                });
+                                handler.save()
+                                    .then(result => {
+                                        console.log(result);
+                                        res.status(201).json({
+                                            message: "Handler created"
+                                        });
+                                    })
+                                    .catch(err => {
+                                        res.status(500).json({
+                                            error: err
+                                        })
+                                    });
+                            }
+                        })
                     }
                 })
-            }
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    });
+                });
         })
         .catch(err => {
             res.status(500).json({
                 error: err
             });
         });
+
+    // Hander.find({
+    //     email: req.body.email
+    // })
+    //     .exec()
+    //     .then(handler => {
+    //         if (handler.length > 0) {
+    //             res.status(409).json({
+    //                 message: "Email is already registered, try logging in!"
+    //             });
+    //         }
+    //         else {
+    //             bcrypt.hash(req.body.password, 10, (err, hash) => {
+    //                 if (err) {
+    //                     return res.status(500).json({
+    //                         error: err
+    //                     });
+    //                 }
+    //                 else {
+    //                     const handler = new Handler({
+    //                         _id: new mongoose.Types.ObjectId,
+    //                         email: req.body.email,
+    //                         password: hash
+    //                     });
+    //                     handler.save()
+    //                         .then(result => {
+    //                             console.log(result);
+    //                             res.status(201).json({
+    //                                 message: "Handler created"
+    //                             });
+    //                         })
+    //                         .catch(err => {
+    //                             res.status(500).json({
+    //                                 error: err
+    //                             })
+    //                         });
+    //                 }
+    //             })
+    //         }
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json({
+    //             error: err
+    //         });
+    //     });
 })
 
+router.post('/login', (req, res, status) => {
+    Handler.find({
+        email: req.body.email
+    })
+        .exec()
+        .then(handler => {
+            if (handler.length < 1) {
+                return res.status(409).json({
+                    message: "Auth failed"
+                })
+            }
+            bcrypt.compare(req.body.password, handler[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: handler[0].email,
+                        handlerId: handler[0]._id
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        });
+                        return res.status(200).json({
+                            message: "Auth sucessfull",
+                            token: token
+                        })
+                }
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
+})
 module.exports = router;
