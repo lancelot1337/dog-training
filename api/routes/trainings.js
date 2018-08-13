f57b1fc41d2747123c1788e1d706aeb20c7ab90f
lancelot1337 <sudhanshusah2001@gmail.com>
2018-08-13 22:25:16 +0530

    Add delete training route and fix typo in trainings controller

--- api/routes/trainings.js
+++ api/routes/trainings.js
@@ -16,8 +16,8 @@ router.get('/', (req, res, next) => {
                         basicObedience: training.basicObedience,
                         specialSkills: training.specialSkills,
                         tracker: JSON.parse(training.tracker),
-                        explosive: JSON.parse(result.explosive),
-                        assault: JSON.parse(result.assault),
+                        explosive: JSON.parse(training.explosive),
+                        assault: JSON.parse(training.assault),
                         request: {
                             type: 'GET',
                             url: 'http://localhost:3000/trainings/' + training._id
@@ -67,5 +67,23 @@ router.post('/', (req, res, next) => {
             res.status(500).json({ error: err });
         });
 });
+
+router.delete('/:id', (req, res, next) => {
+    console.log('/:id DELETE logged');
+    Training.remove({
+        _id: req.params.id
+    })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Training deleted"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+})
 
 module.exports = router;
